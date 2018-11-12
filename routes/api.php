b25c9a241b200afdddb488f372df24249ad1d18b
awarrenm <awarrenmobbs@gmail.com>
2018-11-12 21:43:50 +1100

    Push to Github

---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+/*
+
+Client ID: 1
+Client Secret: owpO4SPTLp3XBWveVT4QvszuovtYsjyTH08ZpLhl
+Password grant client created successfully.
+Client ID: 2
+Client Secret: RrmqrfRh9AP524PZk3JmIe2daM0kWh0PFJhu44xb
+
+
+*/
